91bbfcea7be08b164554b96eddc6d98fdd7f5935
Geisa-mg <geisamorais8@gmail.com>
2022-08-18 08:55:12 -0300

    Fatorial

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
+int fatorial(int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * fatorial(n - 1);
+    }
+}
+
 int main()
 {
     printf("Hello Git\n");
+    printf("Fatorial(5) = %d\n", fatorial(5));
 }
